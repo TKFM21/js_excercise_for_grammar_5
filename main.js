8b853ca788f5df4f79405aa7e290481b96264e31
Fumiaki Takizawa <tkzwfm21@gmail.com>
2019-10-01 01:19:24 +0900

    go pull

--- main.js
+++ main.js
@@ -5,10 +5,13 @@
 //     - 入力: 14, 期待する結果: false
 
 // ここにisOddNumber関数を作る
-
+function isOddNumber(number){
+    return number % 2 === 1;
+}
 
 // ここにconsole.logを使って「9」「14」を引数に渡したときの結果値を出力する
-
+console.log('入力: 9, 期待する結果: true', isOddNumber(9));
+console.log('入力: 14, 期待する結果: false', isOddNumber(14));
 
 
 // 課題2: 引数に渡した「1」以上の整数をつかって階乗の結果を返す「factorial関数」を作る (階乗とは: https://ja.wikipedia.org/wiki/%E9%9A%8E%E4%B9%97)
@@ -22,11 +25,17 @@
 //     3. 【チャレンジ問題】再帰関数を使ったやり方(参考記事: https://qiita.com/chuck0523/items/2c40a5da90a1d73ab956)
 
 // ここにfactorial関数を作る
-
+function factorial(number) {
+    if(number === 1) {
+        return 1;
+    } else {
+        return number * factorial(number - 1);
+    }
+}
 
 // ここにconsole.logを使って「4」「5」を引数に渡したときの結果値を出力する
-
-
+console.log('入力: 4, 期待する結果: 24 (= 4 * 3 * 2 * 1)', factorial(4));
+console.log('入力: 5, 期待する結果: 120 (= 5 * 4 * 3 * 2 * 1)', factorial(5));
 
 // 課題3: 引数に渡した、全ての小文字の文字列の先頭1文字を大文字に変換した文字列を返す「convertFirstCharacterToUpperCase関数」を作る
 //   - アロー関数の形式で作ること
@@ -36,6 +45,10 @@
 //   - 「先頭1文字を大文字にするやり方」の参考記事: https://qiita.com/ichironagata/items/091f8cd629b82b0dcbf8
 
 // ここにconvertFirstCharacterToUpperCase関数を作る
-
+const convertFirstCharacterToUpperCase = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
 
 // ここにconsole.logを使って「'hello'」「'world'」を引数に渡したときの結果値を出力する
+console.log("入力: 'hello', 期待する結果: 'Hello'", convertFirstCharacterToUpperCase('hello'));
+console.log("入力: 'world', 期待する結果: 'World'", convertFirstCharacterToUpperCase('world'));
